08131c4a35b98a7974bac8413b3ddd6db49c4b5a
HEMNATH <146712643+HEMNATH77@users.noreply.github.com>
2024-04-02 21:05:51 +0530

    Add files via upload

---
+++ py programs/text.py
@@ -0,0 +1,31 @@
+print("HELLO FOLKS") 
+
+a=input("HOW ARE TOU MATE??? : ")
+
+if (a =="fine"):
+    b=input("Shall We Start Our Session??? : ")
+    if(b=="yes off course"):
+        print("PROCESSING...")
+    q= input("WHAT IS UNIFIED PROCESS??? : ")    
+    if(q== "it is an iterative and incremental software development"):
+        print("Good")
+    else:
+        print("Sorry.... The Answer is --- It is an Iterative and Incremental Software Development ")    
+    p= input("WHAT IS UML DIAGRAMS??? : ")
+    if(p=="Unified Modelling Language"):
+        print("Perfect")
+    else:
+        print("ohhh Noooo.... IT is Unified Modelling Language ")  
+    e=input("WHAT IS POLYMORPHISM??? : ")
+    if(e=="object's ability to create forms"):
+        print("Well Said")       
+    else:
+        print("NO... IT WILL BE---object's ability to create forms")   
+        print("Test Again...")
+    print("Well you are in good form....see you with more innovative questions....take care ")
+    c=input()
+    if (c=="thankyou"):
+        print("have a good day ")
+else:
+    print("Have A Break And Come Back Stronger") 
+       
