83212018eed2e5770409a6bd8864976fb61cd77e
HEMNATH <146712643+HEMNATH77@users.noreply.github.com>
2024-04-22 11:10:15 +0530

    Update text.py

--- py programs/text.py
+++ py programs/text.py
@@ -1,26 +1,26 @@
 print("HELLO FOLKS") 
 
-a=input("HOW ARE TOU MATE??? : ")
+a=input("HOW ARE YOU MATE??? : ")
 
 if (a =="fine"):
     b=input("Shall We Start Our Session??? : ")
     if(b=="yes off course"):
         print("PROCESSING...")
     q= input("WHAT IS UNIFIED PROCESS??? : ")    
-    if(q== "it is an iterative and incremental software development"):
+    if(q== "iterative  software "):
         print("Good")
     else:
-        print("Sorry.... The Answer is --- It is an Iterative and Incremental Software Development ")    
-    p= input("WHAT IS UML DIAGRAMS??? : ")
+        print("Sorry.... The Answer is -- iterative  software  ")    
+    p= input("WHAT IS UML??? : ")
     if(p=="Unified Modelling Language"):
         print("Perfect")
     else:
         print("ohhh Noooo.... IT is Unified Modelling Language ")  
     e=input("WHAT IS POLYMORPHISM??? : ")
-    if(e=="object's ability to create forms"):
+    if(e=="creating object's forms"):
         print("Well Said")       
     else:
-        print("NO... IT WILL BE---object's ability to create forms")   
+        print("NO... IT WILL BE--- creating object's forms")   
         print("Test Again...")
     print("Well you are in good form....see you with more innovative questions....take care ")
     c=input()
